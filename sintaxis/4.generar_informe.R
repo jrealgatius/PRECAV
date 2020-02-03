5cb950b8fbc1d0df9153e9112f5a9d1a8e177a60
jrealgatius <jordireal@gmail.com>
2020-02-03 17:22:09 +0100

    Actualització: Punts de tall nous en regicor i afegir filtre de dades en render()

--- sintaxis/4.generar_informe.R
+++ sintaxis/4.generar_informe.R
@@ -1,22 +1,23 @@
 library(rmarkdown)
 
-code_dir<-"sintaxis"
-report_filename<-"EDICIO_PRECAV_V3.Rmd"
-report_filename <- file.path(code_dir, report_filename)
-
-output_dir <- "output/reports"
-output <- file.path("..",output_dir)
-render(report_filename, output_dir = output_dir, params = list(output_dir = output))
-
-
-
 
 #### fer corre tota la sintaxi
-funcions_lectura_dades(mostra=T)  # Lectura de 400 000 usuaris / o tota la Població
 
+source(here::here("sintaxis","global_lectura.R"))
+funcions_lectura_dades(mostra=T)  # Lectura de 400 000 usuaris / o tota la Població
 source(here::here("sintaxis","1.LECTURA.R"))
-rmarkdown::render(here::here("sintaxis","2.PREPARACIO.Rmd"))
-rmarkdown::render(here::here("sintaxis","3.RESULTATS.Rmd"))
+
+rmarkdown::render(here::here("sintaxis","2.PREPARACIO.Rmd"),
+                  params=list(evaluar=T,fitxer_entrada="BD_PRECAV_test5.rds",filtre="c_inclusio_grup_MACE==1"))
+
+rmarkdown::render(here::here("sintaxis","3.RESULTATS.Rmd"), output_file="ResultatsMACE")
+
+
+
+
+
+
+
 
 
 
