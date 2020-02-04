9394798c055cbb603c242591489a7d1e75a918f7
jrealgatius <jordireal@gmail.com>
2020-02-04 17:45:36 +0100

    Actualització

--- sintaxis/4.generar_informe.R
+++ sintaxis/4.generar_informe.R
@@ -5,12 +5,17 @@ library(rmarkdown)
 
 source(here::here("sintaxis","global_lectura.R"))
 funcions_lectura_dades(mostra=T)  # Lectura de 400 000 usuaris / o tota la Població
+
+# 1. 
 source(here::here("sintaxis","1.LECTURA.R"))
 
+# 2.
+rm(params)
 rmarkdown::render(here::here("sintaxis","2.PREPARACIO.Rmd"),
-                  params=list(evaluar=T,fitxer_entrada="BD_PRECAV_test5.rds",filtre="c_inclusio_grup_MACE==1"))
+                  params=list(evaluar=T,fitxer_entrada="BD_PRECAV_test5.rds",filtre="c_inclusio_grup_MACE>=0"))
 
-rmarkdown::render(here::here("sintaxis","3.RESULTATS.Rmd"), output_file="ResultatsMACE")
+# 3. 
+rmarkdown::render(here::here("sintaxis","3.RESULTATS.Rmd"), output_file="Resultats_poblacionals")
 
 
 
