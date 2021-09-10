1763947016494fe01202b2ed1ed763e610c2eb9f
jrealgatius <jordireal@gmail.com>
2021-09-10 10:46:05 +0200

    Generar exitus INE

--- codi_emilio/generar_informe.R
+++ codi_emilio/generar_informe.R
@@ -4,13 +4,14 @@ library(rmarkdown)
 
 # 1.
 rmarkdown::render(here::here("codi_emilio","1_lectura_Eortega.Rmd"),
-                  params=list(mostra= FALSE))
+                  params=list(mostra= F))
 
 
 
 # 2.
+
 rmarkdown::render(here::here("codi_emilio","2_preparacio_emilio.Rmd"),
-                  params=list(mostra= FALSE),
-                  output_file=here::here("codi_emilio/resultats","Resultats_poblacionals2"))
+                  params=list(mostra= T),
+                  output_file=here::here("codi_emilio/resultats",paste0("Resultats_",Sys.Date())))
 
 
